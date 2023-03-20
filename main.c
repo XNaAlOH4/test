e47d25d0b8a8715c2d6fe492775a08d39df996b1
XNaAlOH4 <74347686+XNaAlOH4@users.noreply.github.com>
2023-03-20 21:57:04 +0800

    Update main.c
    Added isJPG func

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 
 #define FILE_NAME "G-test.jpg"
 
+#define JPG_SIG 0xE0FFD8FF
+
+char isJPG(char *p) {
+  return JPG_SIG == *(unsigned*)p;
+}
+
 int main() {
   FILE * fp = fopen(FILE_NAME, "rb");
   fseek(fp, 0L, SEEK_END);
@@ -15,6 +21,7 @@ int main() {
   fclose(fp);
 
   printf("file size: %u\n", size);
+  printf("isJPG = %d\n", isJPG(file));
   printf("file-content: %s\n", file);
   
   free(file);
